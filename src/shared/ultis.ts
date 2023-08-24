5923449ca3f5b7890bd1e2dcf67341542c140ded
elvis huanca <JHuanca_21@outlook.com>
2023-08-24 09:40:33 -0500

    formatPrice

--- src/shared/ultis.ts
+++ src/shared/ultis.ts
@@ -1,1 +1,12 @@
 export const itemsPerPage = 5;
+
+export const formatPrice = (price: number) => {
+  return price.toLocaleString("es-PE", {
+    style: "currency",
+    currency: "PEN",
+  });
+  // return price.toLocaleString("en-US", {
+  //   style: "currency",
+  //   currency: "USD", 
+  // });
+}
