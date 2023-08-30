e406a60fafded0cacd728487cb50fe4e368c484b
elvis huanca <JHuanca_21@outlook.com>
2023-08-30 17:56:40 -0500

    third test

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,3 +1,20 @@
+import type { NextApiResponse } from 'next';
+import type { Server as HTTPServer } from 'http'
+import type { Socket as NetSocket } from 'net'
+import type { Server as IOServer } from 'socket.io'
+
+interface SocketServer extends HTTPServer {
+  io?: IOServer | undefined
+}
+
+interface SocketWithIO extends NetSocket {
+  server: SocketServer
+}
+
+export interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO
+}
+
 export enum EStateGeneric {
   IDLE = "idle",
   SUCCEEDED = "succeeded",
@@ -24,7 +41,7 @@ export interface Category {
   name: string;
 }
 
-export interface ICategory{
+export interface ICategory {
   name: string;
 }
 
