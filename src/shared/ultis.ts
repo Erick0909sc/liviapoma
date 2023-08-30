53d1d5dfcfc92f495297912fcf30e0dd9978fb84
elvis huanca <JHuanca_21@outlook.com>
2023-08-30 16:32:25 -0500

    second test

--- src/shared/ultis.ts
+++ src/shared/ultis.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const BASE_URL = process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "https://liviapoma.vercel.app"
+export const BASE_URL = process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "https://liviapoma-git-junior-juniorhuanca.vercel.app/"
+// export const BASE_URL = process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "https://liviapoma.vercel.app"
 
 export const itemsPerPage = 5;
 
