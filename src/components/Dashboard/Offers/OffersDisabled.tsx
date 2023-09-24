086691c492bccde99a197d8e1696b1ad1f9db064
elvis huanca <JHuanca_21@outlook.com>
2023-09-24 14:59:55 -0500

    styles in offers

--- src/components/Dashboard/Offers/OffersDisabled.tsx
+++ src/components/Dashboard/Offers/OffersDisabled.tsx
@@ -32,6 +32,11 @@ const OffersDisabled = (props: Props) => {
   }, [dispatch, offersDisabledStatus]);
   return (
     <div className="grid place-items-center p-1 sm:p-3">
+      {!offersDisabled.length && (
+        <h2 className="text-2xl font-bold mb-4">
+          No hay ofertas desactivadas en este momento
+        </h2>
+      )}
       <div className="grid grid-cols-1 ss:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-7xl">
         {offersDisabled.map((offer, index) => (
           <Card key={index} {...offer} />
