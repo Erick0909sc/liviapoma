88ebad8dfbd949dc570c8ac139d33678e787d98c
elvis huanca <JHuanca_21@outlook.com>
2023-09-26 11:12:46 -0500

    fix layout admin for mobile

--- src/components/Layout/NadvarAdmin/NadvarAmin.tsx
+++ src/components/Layout/NadvarAdmin/NadvarAmin.tsx
@@ -26,59 +26,55 @@ const NadvarAmin: React.FC<NadvarProps> = ({ toggleSidebar, session }) => {
   };
 
   return (
-    <div className="flex">
-      <nav
-        className={`w-full h-14 bg-teal-600  flex text-white items-center transition-all duration-300 `}
-      >
-        <div className="text-white text-[25px] pl-2 flex items-center h-full w-[15%]">
-          <button onClick={toggleSidebarVisibility}>
-            <ImMenu />
-          </button>
-        </div>
+    <nav className={`flex items-center text-white justify-between h-16 bg-teal-700`}>
+      <div className="flex items-center">
+        <button onClick={toggleSidebarVisibility}>
+          <ImMenu className="text-3xl"/>
+        </button>
+      </div>
 
-        <div className=" w-56  lg:w-[25%] flex justify-center">
-          <form className="bg-transparent  border-white border rounded-[10px] w-full  lg:w-[70%]">
-            <input
-              type="text "
-              className="bg-transparent rounded-[10px] p-2 focus:outline-none w-[75%] placeholder-white"
-              placeholder="Ingrese su busqedad"
+      <div className="flex justify-center">
+        <form className="bg-transparent  border-white border rounded-[10px] w-full">
+          <input
+            type="text "
+            className="bg-transparent rounded-[10px] p-2 focus:outline-none w-[75%] placeholder-white"
+            placeholder="Ingrese su busqedad"
+          />
+          <button className="pl-1 border-l text-center" type="button">
+            Buscar
+          </button>
+        </form>
+      </div>
+
+      <div className="flex justify-end text-4xl">
+        {session && (
+          <>
+            <FaUserCircle
+              onClick={toggleModal}
+              className="lg:hidden sm:hidden"
             />
-            <button className="pl-1 w-[25%] border-l text-center" type="button">
-              Buscar
-            </button>
-          </form>
-        </div>
+            {showModal && (
+              <AdminModal isOpen={showModal} onClose={toggleModal} />
+            )}
+          </>
+        )}
+      </div>
 
-        <div className="w-36 flex justify-end pr-3 text-[35px]">
-          {session && (
-            <>
-              <FaUserCircle
-                onClick={toggleModal}
-                className="lg:hidden sm:hidden"
-              />
-              {showModal && (
-                <AdminModal isOpen={showModal} onClose={toggleModal} />
-              )}
-            </>
-          )}
-        </div>
-
-        <div className="w-[60%]  justify-end p-4 gap-3 hidden sm:flex   lg:flex">
-          <button className="text-[25px] ">
-            <GoBellFill />
+      <div className="justify-end gap-3 hidden sm:flex   lg:flex">
+        <button className="text-[25px] ">
+          <GoBellFill />
+        </button>
+        <div className="flex items-center ">
+          <button className="flex gap-2 items-center">
+            <FaUserCircle className="text-[28px]" />
+            <div className="flex items-center">
+              <h2>{session.user.name}</h2>
+              <GoTriangleDown />
+            </div>
           </button>
-          <div className="flex items-center ">
-            <button className="flex gap-2 items-center">
-              <FaUserCircle className="text-[28px]" />
-              <div className="flex items-center">
-                <h2>{session.user.name}</h2>
-                <GoTriangleDown />
-              </div>
-            </button>
-          </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
