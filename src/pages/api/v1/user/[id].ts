436cc6f265b8d84ffd4a2d36543252e9f3e42591
JuniorHuanca <JHuanca_21@outlook.com>
2023-10-20 12:27:41 -0500

    routes for user

--- src/pages/api/v1/user/[id].ts
+++ src/pages/api/v1/user/[id].ts
@@ -8,28 +8,53 @@ export default async function handler(
 ) {
   const { method } = req;
   switch (method) {
-    case "PUT":
+    case "GET":
       try {
         const { id } = req.query;
+        const user = await prisma.user.findUnique({
+          where: { id: id as string },
+        });
+        if (!user) return res.status(404).json({ message: "user not found" });
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+      break;
+    case "PUT":
+      try {
+        const { id, passwordChange } = req.query;
         const findUser = await prisma.user.findUnique({
           where: { id: id as string },
         });
         if (!findUser)
           return res.status(404).json({ message: "user not found" });
 
+        if (passwordChange) {
+          const {
+            password,
+          }: {
+            password: string;
+          } = req.body;
+          const passwordhash = await hash(password, 5);
+          const user = await prisma.user.update({
+            where: {
+              id: findUser.id,
+            },
+            data: {
+              password: passwordhash,
+            },
+          });
+          return res.status(200).json(user);
+        }
         const {
           name,
           email,
-          password,
           image,
         }: {
           name: string;
           email: string;
-          password: string;
           image: string;
         } = req.body;
-
-        const passwordhash = await hash(password, 5);
 
         const user = await prisma.user.update({
           where: {
@@ -38,7 +63,6 @@ export default async function handler(
           data: {
             name,
             email,
-            password: passwordhash,
             image,
           },
         });
